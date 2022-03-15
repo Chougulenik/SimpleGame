922209c5c109221610c53f069e3a6523f5b9cfa4
Chougulenik <nikitachougule47@gmail.com>
2022-03-15 13:28:31 -0400

    Simple game is added

---
+++ src/com/simplegame/Main.java
@@ -0,0 +1,20 @@
+package com.simplegame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Player1 player = new Player1("nikita","sword",100);
+//		System.out.println(player.getName());
+//		System.out.println(player.getHealth());
+//		System.out.println(player.getWeapon());
+		
+		player.damageByGun1();
+		player.damageByGun1();
+		player.damageByGun2();
+		player.heal();
+
+		
+		
+	}
+
+}
